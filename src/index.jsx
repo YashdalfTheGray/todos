cfad1c135c9e0ca8c8c59bb876da7f9252f4f519
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-08-22 08:42:15 -0700

    added react-hot-loader to the app

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
+import { hot } from 'react-hot-loader';
 
-const App = () => (
+const App = hot(module)(() => (
   <Button variant="contained" color="primary">
     Hello World
   </Button>
-);
+));
 
 ReactDOM.render(<App />, document.querySelector('div#app-root'));
