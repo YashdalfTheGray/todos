5c9c8cfc270fb0cd168c7563bb815b32a12d8865
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-08-23 09:14:01 -0700

    basically and hmr test

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 import { hot } from 'react-hot-loader';
 
 const App = hot(module)(() => (
-  <Button variant="contained" color="primary">
+  <Button variant="contained" color="secondary">
     Hello World
   </Button>
 ));
