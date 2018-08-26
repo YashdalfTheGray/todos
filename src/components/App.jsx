98a015db0ce35d9e6280f62ac7ccc4b751adf5f9
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-08-26 15:57:02 -0700

    added another button, got the styling to work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -13,16 +17,23 @@ export default class App extends React.Component {
   render() {
     const { title } = this.state;
     const styles = {
-      root: { flexGrow: 1 }
+      root: { flexGrow: 1 },
+      toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }
     };
-
     return (
       <div className={styles.root}>
         <AppBar position="static" color="primary">
-          <Toolbar>
+          <Toolbar style={{ ...styles.toolbar }}>
             <Typography variant="title" color="inherit">
               {title}
             </Typography>
+            <IconButton color="inherit" aria-label="Add">
+              <AddIcon />
+            </IconButton>
           </Toolbar>
         </AppBar>
       </div>
