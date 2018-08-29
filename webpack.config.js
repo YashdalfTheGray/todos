0056069b166f6c0e744756124a6194971c4f15ed
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-08-29 10:27:29 -0700

    added injecting the firebase stuff, polyfill and devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const { resolve } = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_MESSAGING_ID
+} = process.env;
+
 module.exports = {
-  entry: ['./src/index.jsx'],
+  entry: ['@babel/polyfill', './src/index.jsx'],
   output: {
     path: resolve(__dirname, './public'),
     filename: 'bundle.js'
@@ -12,6 +19,7 @@ module.exports = {
     contentBase: resolve(__dirname, './public'),
     hot: true
   },
+  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -23,7 +31,12 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['public/*.js']),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      FIREBASE_API_KEY: JSON.stringify(FIREBASE_API_KEY),
+      FIREBASE_PROJECT_ID: JSON.stringify(FIREBASE_PROJECT_ID),
+      FIREBASE_MESSAGING_ID: JSON.stringify(FIREBASE_MESSAGING_ID)
+    })
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.css', '.scss']
