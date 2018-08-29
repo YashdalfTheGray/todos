4dc9624af95dc0868ae8edbd727021c9ff0ef63d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-08-29 10:27:49 -0700

    trying to get data from firebase but it's not working

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { hot } from 'react-hot-loader';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 import App from './components/App';
+
+firebase.initializeApp({
+  apiKey: FIREBASE_API_KEY,
+  authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: `${FIREBASE_PROJECT_ID}`,
+  messagingSenderId: FIREBASE_MESSAGING_ID
+});
+
+(async () => {
+  const snapshot = await firebase
+    .database()
+    .ref('todos')
+    .once('value');
+  console.log(snapshot.val());
+})();
 
 const AppFrame = hot(module)(() => (
   <React.Fragment>
