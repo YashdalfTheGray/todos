824f1142c1649a10b177df51097162adb291a571
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-09-08 21:46:30 -0700

    pulled the store in and wrap app in provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { hot } from 'react-hot-loader';
 
 import App from './components/App';
 import initFirebase from './firebase';
+import store from './redux';
 
 initFirebase();
 
 const AppFrame = hot(module)(() => (
   <React.Fragment>
     <CssBaseline>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </CssBaseline>
   </React.Fragment>
 ));
