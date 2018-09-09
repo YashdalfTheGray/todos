d5abcf3ce6715e31c8b706cb826408f2e7b6992a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-09-09 03:55:17 -0700

    wired up the dev tools extension

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from './reducers';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 
 sagaMiddleware.run(rootSaga);
 
