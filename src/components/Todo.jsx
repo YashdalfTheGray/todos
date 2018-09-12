4af9db0e1c869514fc9bc6a140a8bef55588c47d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-09-12 08:12:56 -0700

    started making the todo a card

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 import { TodoPropType } from '../customProps';
 
-const Todo = ({ todo }) => <pre>{JSON.stringify(todo, null, 2)}</pre>;
+const Todo = ({ todo }) => (
+  <Card>
+    <CardContent>
+      <Typography variant="headline" component="h2">
+        {todo.content}
+      </Typography>
+      <pre>{JSON.stringify(todo, null, 2)}</pre>
+    </CardContent>
+    <CardActions>
+      <Button color="primary">Mark done</Button>
+    </CardActions>
+  </Card>
+);
 
 Todo.propTypes = {
   todo: TodoPropType.isRequired
