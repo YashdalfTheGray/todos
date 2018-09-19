7fd7690512eb5e769bf15c509d8e5cbe813b0a86
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-09-19 08:30:44 -0700

    todo update wiring

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as moment from 'moment';
+import * as PropTypes from 'prop-types';
 
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
@@ -9,30 +10,61 @@ import Typography from '@material-ui/core/Typography';
 
 import TodoPropType from '../customProps';
 
-const Todo = ({ todo }) => (
-  <Card>
-    <CardContent>
-      <Typography variant="headline" component="h2">
-        {todo.content}
-      </Typography>
-      <Typography variant="caption">
-        Created&nbsp;
-        {moment(todo.createdAt).fromNow()}
-      </Typography>
-      <Typography variant="caption">
-        Last modified&nbsp;
-        {moment(todo.modifiedAt).fromNow()}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button>Edit</Button>
-      <Button color="primary">Mark done</Button>
-    </CardActions>
-  </Card>
-);
+class Todo extends React.Component {
+  propTypes = {
+    todo: TodoPropType.isRequired,
+    onUpdate: PropTypes.func.isRequired
+  };
 
-Todo.propTypes = {
-  todo: TodoPropType.isRequired
-};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editMode: true
+    };
+  }
+
+  handleDoneClick = () => {
+    const { onUpdate, todo } = this.props;
+
+    onUpdate(todo.id);
+  };
+
+  render() {
+    const { todo } = this.props;
+    const { editMode } = this.state;
+
+    return (
+      <Card>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            {todo.content}
+          </Typography>
+          <Typography variant="caption">
+            Created&nbsp;
+            {moment(todo.createdAt).fromNow()}
+          </Typography>
+          <Typography variant="caption">
+            Last modified&nbsp;
+            {moment(todo.modifiedAt).fromNow()}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          {(() => {
+            if (editMode) {
+              return <Button onClick={this.handleDoneClick}>Done</Button>;
+            }
+            return (
+              <>
+                <Button>Edit</Button>
+                <Button color="primary">Mark done</Button>
+              </>
+            );
+          })()}
+        </CardActions>
+      </Card>
+    );
+  }
+}
 
 export default Todo;
