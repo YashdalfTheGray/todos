75116738866f87b6f849449f31c0158d51d51d97
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-09-22 19:29:37 -0700

    found a silly bug, fixed it

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,5 +2,7 @@ export const getAllTodoIds = store => store.todoIds;
 export const getAllTodos = store => Object.values(store.todosById);
 export const getTodoById = (store, id) => store.todosById[id];
 export const getVisibility = store => store.visibility;
-export const getApiErrorByApi = (store, api) => store[api].isProcessing;
-export const getIsProcessingByApi = (store, api) => store[api].apiError;
+export const getApiErrorByApi = (store, api) =>
+  store[`${api}ApiProcessing`].apiError;
+export const getIsProcessingByApi = (store, api) =>
+  store[`${api}ApiProcessing`].isProcessing;
