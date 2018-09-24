78d6de60f22dc3fdc2badf0920598802666e0e2a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-09-24 08:27:24 -0700

    get all todos loading message wiring

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import { withStyles } from '@material-ui/core/styles';
+import Snackbar from '@material-ui/core/Snackbar';
 
 import * as selectors from '../redux/selectors';
 import todoActions from '../redux/actions';
@@ -46,35 +47,87 @@ class TodoList extends React.Component {
     getAllTodos: PropTypes.func.isRequired,
     updateTodo: PropTypes.func.isRequired,
     classes: PropTypes.object.isRequired,
-    isProcessingUpdate: PropTypes.bool.isRequired
+    isProcessingUpdate: PropTypes.bool.isRequired,
+    isProcessingGetAllTodos: PropTypes.bool.isRequired
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      snackbarMessage: '',
+      snackbarVisible: false
+    };
+  }
 
   componentDidMount() {
     const { getAllTodos } = this.props;
     getAllTodos();
   }
 
+  componentDidUpdate(prevProps) {
+    const { isProcessingGetAllTodos } = this.props;
+    const { isProcessingGetAllTodos: prevProcessingGetAllTodos } = prevProps;
+
+    /* eslint-disable react/no-did-update-set-state */
+    if (!prevProcessingGetAllTodos && isProcessingGetAllTodos) {
+      this.setState({
+        snackbarMessage: 'Loading...',
+        snackbarVisible: true
+      });
+    } else if (prevProcessingGetAllTodos && !isProcessingGetAllTodos) {
+      this.setState({
+        snackbarMessage: '',
+        snackbarVisible: false
+      });
+    }
+    /* eslint-enable react/no-did-update-set-state */
+  }
+
+  handleClose = () => {
+    this.setState({
+      snackbarVisible: false
+    });
+  };
+
   render() {
     const { todos, classes, updateTodo, isProcessingUpdate } = this.props;
+    const { snackbarMessage, snackbarVisible } = this.state;
+
     return (
-      <div className={classes.root}>
-        <GridList
-          className={classes.gridList}
-          cols={3}
-          spacing={8}
-          cellHeight="auto">
-          {todos.map(t => (
-            <GridListTile key={t.id}>
-              <Todo
-                key={t.id}
-                todo={t}
-                onUpdate={updateTodo}
-                isProcessingUpdate={isProcessingUpdate}
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
+      <>
+        <div className={classes.root}>
+          <GridList
+            className={classes.gridList}
+            cols={3}
+            spacing={8}
+            cellHeight="auto">
+            {todos.map(t => (
+              <GridListTile key={t.id}>
+                <Todo
+                  key={t.id}
+                  todo={t}
+                  onUpdate={updateTodo}
+                  isProcessingUpdate={isProcessingUpdate}
+                />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          open={snackbarVisible}
+          autoHideDuration={null}
+          onClose={this.handleClose}
+          ContentProps={{
+            'aria-describedby': 'message-id'
+          }}
+          message={<span id="message-id">{snackbarMessage}</span>}
+        />
+      </>
     );
   }
 }
