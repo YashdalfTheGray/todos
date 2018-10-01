b08b557986d0fdd7239729e7d85c1c838c478fec
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-10-01 08:54:53 -0700

    added a fab for add rather than a toolbar action"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,32 @@ import * as React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import withStyles from '@material-ui/core/styles/withStyles';
+import * as PropTypes from 'prop-types';
 
 import TodoList from './TodoList';
 
-export default class App extends React.Component {
+const appStyles = theme => ({
+  root: { flexGrow: 1 },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing.unit * 2,
+    right: theme.spacing.unit * 2
+  }
+});
+
+class App extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired
+  };
+
   constructor(props) {
     super(props);
 
@@ -18,28 +38,24 @@ export default class App extends React.Component {
 
   render() {
     const { title } = this.state;
-    const styles = {
-      root: { flexGrow: 1 },
-      toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-      }
-    };
+    const { classes } = this.props;
+
     return (
-      <div className={styles.root}>
+      <div className={classes.root}>
         <AppBar position="static" color="primary">
-          <Toolbar style={{ ...styles.toolbar }}>
+          <Toolbar className={classes.toolbar}>
             <Typography variant="title" color="inherit">
               {title}
             </Typography>
-            <IconButton color="inherit" aria-label="Add">
-              <AddIcon />
-            </IconButton>
           </Toolbar>
         </AppBar>
         <TodoList />
+        <Button variant="fab" className={classes.fab} color="secondary">
+          <AddIcon />
+        </Button>
       </div>
     );
   }
 }
+
+export default withStyles(appStyles)(App);
