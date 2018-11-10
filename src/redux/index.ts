2c276dd315f9a0f32a8bef277ca91b0b076eacc5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-11-10 06:52:38 -0800

    changed all of the redux arch to typescript

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducers';
 import rootSaga from './sagas';
