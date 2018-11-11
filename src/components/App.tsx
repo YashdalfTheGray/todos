7e6279cb58c59f5a0fa79287096b6f9c3d7b88cc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-11-11 08:21:55 -0800

    this should finish out building the app in typescript

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,28 @@
 import * as React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
-import withStyles from '@material-ui/core/styles/withStyles';
-import * as PropTypes from 'prop-types';
-import Switch from '@material-ui/core/Switch';
+
 import { connect } from 'react-redux';
 
-import TodoList from './TodoList';
-import AddTodo from './AddTodo';
-import todoActions from '../redux/actions';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import createStyles from '@material-ui/core/styles/createStyles';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import Switch from '@material-ui/core/Switch';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
+
+import { TodoActions } from '../redux/actions';
+import { Visibility } from '../redux/constants';
+import ITodoStoreShape from '../redux/ITodosStore';
 import * as todoSelectors from '../redux/selectors';
-import constants from '../redux/constants';
+
+import AddTodo from './AddTodo';
+import TodoList from './TodoList';
 
 const createTodoSlice = 'createTodo';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: ITodoStoreShape) => ({
   isProcessingCreateTodo: todoSelectors.getIsProcessingByApi(
     state,
     createTodoSlice
@@ -27,36 +32,37 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-  createTodo: todoActions.createTodo,
-  setVisibilityAll: todoActions.setVisibilityAll,
-  setVisibilityOpen: todoActions.setVisibilityOpen
+  createTodo: TodoActions.createTodo,
+  setVisibilityAll: TodoActions.setVisibilityAll,
+  setVisibilityOpen: TodoActions.setVisibilityOpen
 };
 
-const appStyles = theme => ({
-  root: { flexGrow: 1 },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  fab: {
-    position: 'absolute',
-    bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 2
-  }
-});
+const appStyles = (theme: Theme) =>
+  createStyles({
+    root: { flexGrow: 1 },
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    fab: {
+      position: 'absolute',
+      bottom: theme.spacing.unit * 2,
+      right: theme.spacing.unit * 2
+    }
+  });
 
-class App extends React.Component {
-  static propTypes = {
-    classes: PropTypes.object.isRequired,
-    createTodo: PropTypes.func.isRequired,
-    setVisibilityAll: PropTypes.func.isRequired,
-    setVisibilityOpen: PropTypes.func.isRequired,
-    visibility: PropTypes.oneOf([...Object.values(constants.visibility)])
-      .isRequired
-  };
+type AppProps = ReturnType<typeof mapStateToProps> &
+  typeof mapDispatchToProps &
+  WithStyles<typeof appStyles>;
 
-  constructor(props) {
+interface IAppState {
+  title: string;
+  isAddDialogOpen: boolean;
+}
+
+class App extends React.Component<AppProps, IAppState> {
+  constructor(props: AppProps) {
     super(props);
 
     this.state = {
@@ -65,13 +71,13 @@ class App extends React.Component {
     };
   }
 
-  handleTodoAdd = () => {
+  public handleTodoAdd = () => {
     this.setState({
       isAddDialogOpen: true
     });
   };
 
-  handleDialogClose = text => {
+  public handleDialogClose = (text?: string) => {
     const { createTodo } = this.props;
 
     this.setState({
@@ -83,17 +89,17 @@ class App extends React.Component {
     }
   };
 
-  handleChange = () => {
+  public handleChange = () => {
     const { setVisibilityAll, setVisibilityOpen, visibility } = this.props;
 
-    if (visibility === constants.visibility.all) {
+    if (visibility === Visibility.ALL) {
       setVisibilityOpen();
     } else {
       setVisibilityAll();
     }
   };
 
-  render() {
+  public render() {
     const { title, isAddDialogOpen } = this.state;
     const { classes, visibility } = this.props;
 
@@ -113,7 +119,7 @@ class App extends React.Component {
               <Typography color="inherit">All</Typography>
               <Switch
                 data-test-id="visibility-toggle"
-                checked={visibility === constants.visibility.open}
+                checked={visibility === Visibility.OPEN}
                 onChange={this.handleChange}
               />
               <Typography color="inherit">Open</Typography>
